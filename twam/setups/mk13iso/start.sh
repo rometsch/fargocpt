b888c47ca016b183d9a3c181b85f64bf95261ab5
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-06-21 07:56:42 +0200

    move setup to setups folder as in fargo3d

---
+++ twam/setups/mk13iso/start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source env_sheep
+
+# BINAC: Change dir
+if [[ ${PBS_O_WORKDIR} ]]
+then
+	cd $PBS_O_WORKDIR
+	# Load cuda when on a gpu node
+	if [[ $(hostname -s) = gpu* ]]
+	then
+		module load devel/cuda/8.0
+		unset CUDA_VISIBLE_DEVICES
+	fi
+fi
+
+mpirun -n $NCPU fargo setups/$SETUP/in.par
