583363699a4737e95275faf4eff94d2cd9a11d81
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-06-21 07:57:03 +0200

    use a sheeprc file for configuration

--- twam/queue.sh
+++ twam/queue.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
+source env_sheep
+
 case $(hostname -s) in
 	login0*)
-		# We are on binac
-		module load devel/cuda/8.0
 		qsub -N "$(cat uuid)" \
 			 -e err.log \
 			 -o out.log \
 			 -m bae \
 			 -M "thomas.rometsch@uni-tuebingen.de" \
 			 -l walltime=03:00:00 \
-			 -l nodes=1:ppn=1:gpus=1:exclusive_process \
-			 -q gpu \
+			 -l nodes=1:ppn=$NCPU \
+			 -q $QUEUE \
 			 $1
 		;;
-	cpt-titan)
-		bsub -o out.log -e err.log -q default $PWD/$1
+	*)
+	    $PWD/$1 1>out.log 2>err.log &
 		;;
-
-	cpt-*)
-		bsub -o out.log -e err.log -q gpu0 $PWD/$1
-		;;
-  	*)
-  		;;
 esac
