1cedbcac3c1edebdd23cce45f9bec7ff1664ec6e
Lucas Jordan <lucas.jordan@student.uni-tuebingen.de>
2018-10-25 15:14:46 +0200

    Revert "Revert "Revert "Print grid data to out/dimensions.dat (Lucas)"""
    This reverts commit 71a6c6e8d1641ade51d0c82cd7839abfd435f031.

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,6 @@ int main(int argc, char* argv[])
 
 	ReadVariables(options::parameter_file, data);
 	parameters::summarize_parameters();
-	parameters::write_grid_data_to_file();
 
 	units::print_code_units();
 	units::write_code_unit_file();
