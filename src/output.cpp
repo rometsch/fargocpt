7b3289f0e3a71d3552ff011085bb7ce7a5a42297
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-03-22 14:19:48 +0100

    remove lost mass from misc file, now version 2

--- src/output.cpp
+++ src/output.cpp
@@ -228,7 +228,9 @@ void write_misc(unsigned int timestep)
 
 		if (!fd_created) {
 			// print header
-			fprintf(fd,"# timestep\tphysical time\tOmegaFrame\tLostMass\tFrameAngle\n");
+			fprintf(fd,"#FargoCPT misc file\n ");
+			fprintf(fd,"#version: 2\n ");
+			fprintf(fd,"#content: timestep\tphysical time\tOmegaFrame\tFrameAngle\n");
 			fd_created = true;
 		}
 	}
