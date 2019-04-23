d9817e0e6235e2d509981e6b77dfbdd0568ad4fc
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-04-23 17:38:33 +0200

    add readme for binary io test

--- test/binary_io/test.sh
+++ test/binary_io/test.sh
@@ -15,5 +15,7 @@ cp $ODIR/gasdens$N.dat $ODIR/gasdens$N.dat.bak
 mpirun ./fargo $SDIR/fargo.par -r $N
 # compare the results
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "comparing results"
-cmp -b $ODIR/gasdens$N.dat $ODIR/gasdens$N.dat.bak
+echo "comparing results (should be empty for identical files)"
+cmp -b $ODIR/gasdens$N.dat.bak $ODIR/gasdens$N.dat
+echo "do statistics"
+$SDIR/print_differences.py $ODIR/gasdens$N.dat.bak $ODIR/gasdens$N.dat
