fa99453afc55ef31ade2b4ccaa146b97cc41b7ea
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-04-23 17:27:29 +0200

    test for binary input output

---
+++ test/binary_io/print_diffrences.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file1')
+parser.add_argument('file2')
+args = parser.parse_args()
+
+file1 = args.file1
+file2 = args.file2
+
+data1 = np.fromfile(file1)
+data2 = np.fromfile(file2)
+
+delta = np.abs(data1-data2)
+Ndiff = np.sum(delta!=0)
+Ntot = len(data1)
+
+delta = delta[ delta!=0 ]
+
+print("Number of doubles = ", Ntot)
+print("Number of different values = ", Ndiff)
+print("min value (file1) = ", np.min(data1))
+print("max value (file1) = ", np.max(data1))
+print("min diff = ", np.min(delta))
+print("max diff = ", np.max(delta))
+print("avg diff = ", np.mean(delta))
+print("median diff = ", np.median(delta))
---
+++ test/binary_io/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# test whether outputs are the exact same after reading and writing output files
+if [ "$#" -eq 1 ]; then
+	N=$1
+else
+	N=1
+fi
+ODIR=outputs/binary_io_test
+SDIR=test/binary_io
+# change number of total output steps to selected N
+./Tools/chprm.py $SDIR/fargo.par Ntot $N
+rm -rf $ODIR
+mpirun ./fargo $SDIR/fargo.par
+cp $ODIR/gasdens$N.dat $ODIR/gasdens$N.dat.bak
+mpirun ./fargo $SDIR/fargo.par -r $N
+# compare the results
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "comparing results"
+cmp -b $ODIR/gasdens$N.dat $ODIR/gasdens$N.dat.bak
