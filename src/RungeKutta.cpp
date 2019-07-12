24383ef7e9ebec50c32fcb89fb3e8f1da2ec34b7
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-07-12 16:47:04 +0200

    fix int/unsigned ints in Runge-Kutta scheme

--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -10,11 +10,10 @@
 static double k1[MAX1D], k2[MAX1D], k3[MAX1D], k4[MAX1D], k5[MAX1D], k6[MAX1D];
 static double Dist[MAX1D];
 
-void DerivMotionRK5(double* q_init, double* masses, double* deriv, int n, double dt, int* feelothers)
+void DerivMotionRK5(double* q_init, double* masses, double* deriv,unsigned int n, double dt, int* feelothers)
 {
 	double *x,*y,*vx,*vy, dist;
 	double *deriv_x, *deriv_y, *deriv_vx, *deriv_vy;
-	int i, j;
 	double indirect_x = 0.0;
 	double indirect_y = 0.0;
 
@@ -52,25 +51,24 @@ void DerivMotionRK5(double* q_init, double* masses, double* deriv, int n, double
 			}
 		}
 	}
-	for (i = 0; i < 4*n; i++)
+
+	// multiply all derivatives by dt
+	for (unsigned int i = 0; i < 4*n; i++)
 		deriv[i] *= dt;
 }
 
-void TranslatePlanetRK5(double* qold, double c1, double c2, double c3, double c4, double c5, double* qnew, int n)
+void TranslatePlanetRK5(double* qold, double c1, double c2, double c3, double c4, double c5, double* qnew,unsigned int n)
 {
-	int i;
-
-	for (i = 0; i < 4*n; i++)
+	for (unsigned int i = 0; i < 4*n; i++)
 		qnew[i] = qold[i]+c1*k1[i]+c2*k2[i]+c3*k3[i]+c4*k4[i]+c5*k5[i];
 }
 
-void RungeKutta(double* q0, double dt, double* masses, double* q1, int n, int* feelothers)
+void RungeKutta(double* q0, double dt, double* masses, double* q1,unsigned int n, int* feelothers)
 {
-	int i;
 	double timestep;
 
 	timestep = dt;
-	for (i = 0; i < n*4; i++) {
+	for (unsigned int i = 0; i < n*4; i++) {
 		k1[i] = k2[i] = k3[i] = k4[i] = k5[i] = k6[i];
 	}
 	DerivMotionRK5(q0, masses, k1, n, timestep, feelothers);
@@ -84,7 +82,7 @@ void RungeKutta(double* q0, double dt, double* masses, double* q1, int n, int* f
 	DerivMotionRK5(q1, masses, k5, n, timestep, feelothers);
 	TranslatePlanetRK5(q0, 1631.0/55296.0, 175.0/512.0, 575.0/13824.0, 44275.0/110592.0, 253.0/4096.0, q1, n);
 	DerivMotionRK5(q1, masses, k6, n, timestep, feelothers);
-	for (i = 0; i < 4*n; i++) {
+	for (unsigned int i = 0; i < 4*n; i++) {
 		q1[i]=q0[i]+37.0/378.0*k1[i]+250.0/621.0*k3[i]+125.0/594.0*k4[i]+512.0/1771.0*k6[i];
 	}
 }
