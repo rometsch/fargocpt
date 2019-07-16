6bf3dac3b7996733368ba4757c6dfb1fa22736fe
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-07-16 18:54:54 +0200

    fix calculation of velocity for first object

--- src/planetary_system.cpp
+++ src/planetary_system.cpp
@@ -281,7 +281,11 @@ void t_planetary_system::initialize_planet_jacobi(t_planet *planet, double mass,
 	double x = com.x + r*cos_ota;
 	double y = com.y + r*sin_ota;
 
-	double v = sqrt( (com_mass + mass) / (semi_major_axis*(1+eccentricity*eccentricity)) );
+	double v = 0.0;
+	if (semi_major_axis > 0.0) {
+		v = sqrt( (com_mass + mass) / (semi_major_axis*(1+eccentricity*eccentricity)) );
+	}
+
 	double vx = v*( -cos_o*sin_ta - sin_o*(eccentricity + cos_ta) );
 	double vy = v*( -sin_o*sin_ta + cos_o*(eccentricity + cos_ta) );
 
