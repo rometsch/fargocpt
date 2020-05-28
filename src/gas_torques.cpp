aedbdfc865ca33bbb6992f12dfc1c8fa35532e12
Lucas Jordan <lucas.jordan@student.uni-tuebingen.de>
2020-05-28 15:06:15 +0200

    Fixed missing include.

---
+++ src/gas_torques.cpp
@@ -0,0 +1,167 @@
+#include "gas_torques.h"
+#include "data.h"
+#include "global.h"
+#include "constants.h"
+
+#include <cmath>
+
+namespace gas_torques
+{
+
+
+unsigned int get_next_azimuthal_id(const unsigned int id)
+{
+	unsigned int id_next = id + 1;
+	if(id_next == NAzimuthal) {
+		return 0;
+	}
+	else{
+		return id_next;
+	}
+}
+
+unsigned int get_prev_azimuthal_id(const unsigned int id)
+{
+	if(id == 0){
+		return  NAzimuthal -1;
+	}
+	else{
+		return id - 1;
+	}
+}
+
+
+
+void calculate_advection_torque(t_data &data, const double dt)
+{
+
+	t_polargrid &t_adv = data[t_data::ADVECTION_TORQUE];
+	t_polargrid &vphi = data[t_data::V_AZIMUTHAL];
+	t_polargrid &vr = data[t_data::V_RADIAL];
+	t_polargrid &dens = data[t_data::DENSITY];
+
+	for (unsigned int n_r = 0; n_r <= t_adv.get_max_radial(); ++n_r) {
+
+		const double r = Rmed[n_r];
+		const double inv_dr = InvDiffRsup[n_r];
+
+	for (unsigned int n_az = 0; n_az <= t_adv.get_max_azimuthal(); ++n_az) {
+		const double sigma_cell = dens(n_r, n_az);
+
+		const double vr_top = vr(n_r+1, n_az);
+		const double vr_bot = vr(n_r, n_az);
+
+		// average to cell center
+		double vr_cell = (Rmed[n_r]-Rinf[n_r])*vr_top + (Rsup[n_r]-Rmed[n_r])*vr_bot;
+		vr_cell *= inv_dr;
+
+		const double vphi_cell_left = vphi(n_r, n_az);
+		const double vphi_cell_right = vphi(n_r, get_next_azimuthal_id(n_az));
+		const double vphi_cell = 0.5*(vphi_cell_left + vphi_cell_right);
+
+		t_adv(n_r, n_az) += -std::pow(r, 2.0) * sigma_cell * vr_cell * vphi_cell * dt;
+	}
+	}
+}
+
+
+
+
+
+void calculate_viscous_torque(t_data &data, const double dt)
+{
+
+	t_polargrid &t_visc = data[t_data::VISCOUS_TORQUE];
+	t_polargrid &visc = data[t_data::VISCOSITY];
+	t_polargrid &vphi = data[t_data::V_AZIMUTHAL];
+	t_polargrid &vr = data[t_data::V_RADIAL];
+	t_polargrid &dens = data[t_data::DENSITY];
+
+	const double invdphi = (double)t_visc.get_size_azimuthal() /(2.0 * M_PI);
+
+	for (unsigned int n_r = 1; n_r < t_visc.get_max_radial(); ++n_r) {
+
+		const double r = Rmed[n_r];
+		const double inv_dr = InvDiffRsup[n_r];
+		const double inv_dr_med_top = InvDiffRmed[n_r+1];
+		const double inv_dr_med_bot = InvDiffRmed[n_r];
+
+	for (unsigned int n_az = 0; n_az < t_visc.get_max_azimuthal(); ++n_az) {
+		const double sigma_cell = dens(n_r, n_az);
+		const double viscosity_cell = visc(n_r, n_az);
+
+
+		const double vr_cellm1_top = vr(n_r+1, get_prev_azimuthal_id(n_az));
+		const double vr_cellp1_top = vr(n_r+1, get_next_azimuthal_id(n_az));
+
+		const double dvr_dphi_top = (vr_cellp1_top - vr_cellm1_top) * 0.5 * invdphi;
+
+		const double vr_cellm1 = vr(n_r, get_prev_azimuthal_id(n_az));
+		const double vr_cellp1 = vr(n_r, get_next_azimuthal_id(n_az));
+
+		const double dvr_dphi_bot = (vr_cellp1 - vr_cellm1) * 0.5 * invdphi;
+
+		// average to cell center
+		double dvr_dphi = (Rmed[n_r]-Rinf[n_r])*dvr_dphi_top + (Rsup[n_r]-Rmed[n_r])*dvr_dphi_bot;
+		dvr_dphi *= inv_dr;
+
+
+		const double vphi_cellm1_top = vphi(n_r+1, n_az);
+		const double vphi_cellp1_top = vphi(n_r+1, get_next_azimuthal_id(n_az));
+		const double phi_dot_top = 0.5*(vphi_cellp1_top + vphi_cellm1_top)/Rmed[n_r+1];
+
+		const double vphi_cellm1 = vphi(n_r, n_az);
+		const double vphi_cellp1 = vphi(n_r, get_next_azimuthal_id(n_az));
+		const double phi_dot = 0.5*(vphi_cellp1 + vphi_cellm1)/Rmed[n_r];
+
+		const double vphi_cellm1_bot = vphi(n_r-1, n_az);
+		const double vphi_cellp1_bot = vphi(n_r-1, get_next_azimuthal_id(n_az));
+		const double phi_dot_bot = 0.5*(vphi_cellp1_bot + vphi_cellm1_bot)/Rmed[n_r-1];
+
+		// derivatives at the center of top and bottom interface
+		const double dphi_dot_dr_top = (phi_dot_top - phi_dot)*inv_dr_med_top;
+		const double dphi_dot_dr_bot = (phi_dot - phi_dot_bot)*inv_dr_med_bot;
+
+		// average to cell center
+		double dphi_dot_dr = (Rmed[n_r]-Rinf[n_r])*dphi_dot_dr_top + (Rsup[n_r]-Rmed[n_r])*dphi_dot_dr_bot;
+		dphi_dot_dr *= inv_dr;
+
+		t_visc(n_r, n_az) += -std::pow(r,3.0) * viscosity_cell * sigma_cell * (dphi_dot_dr + 1.0/(std::pow(r,2.0)) * dvr_dphi) * dt/DT;
+
+	}
+	}
+}
+
+
+/**
+ * @brief calculate_gravitational_torque, see Eq. (32) in Miranda 2017.
+ * @param data
+ * @param dt
+ */
+void calculate_gravitational_torque(t_data &data, const double dt)
+{
+
+	t_polargrid &t_grav = data[t_data::GRAVITATIONAL_TORQUE_NOT_INTEGRATED];
+	t_polargrid &pot_grav = data[t_data::POTENTIAL];
+
+	const double invdphi = (double)t_grav.get_size_azimuthal() /(2.0 * PI);
+
+	for (unsigned int n_r = 0; n_r <= t_grav.get_max_radial(); ++n_r) {
+
+	const double r = Rmed[n_r];
+	const double dr = (Rsup[n_r] - Rinf[n_r]);
+
+	for (unsigned int n_az = 0; n_az <= t_grav.get_max_azimuthal(); ++n_az) {
+		const double sigma_cell = data[t_data::DENSITY](n_r, n_az);
+
+		//dPhi/dphi
+		//const double gradphi_minus = (pot_grav(n_r, n_az) - pot_grav(n_r, n_az == 0 ? pot_grav.get_max_azimuthal() : n_az - 1))*invdphi;
+		//const double gradphi = (pot_grav(n_r, n_az == pot_grav.get_max_azimuthal() ? 0 : n_az+1) - pot_grav(n_r, n_az))*invdphi;
+		const double gradphi = (pot_grav(n_r, get_next_azimuthal_id(n_az)) - pot_grav(n_r, get_prev_azimuthal_id(n_az)))*invdphi*0.5;
+		t_grav(n_r, n_az) += -r * sigma_cell * gradphi * dr * dt/DT;
+	}
+	}
+
+}
+
+} // namespace gas_torques
