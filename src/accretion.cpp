02cdc7cca3b111264c7ddeb7d550e0de1037ab69
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2022-06-22 14:55:33 +0200

    Changed hill_azimuthal_index to allow full 2 pi accretion

--- src/accretion.cpp
+++ src/accretion.cpp
@@ -45,10 +45,11 @@ static std::tuple<int, int> hill_azimuthal_index(const double angle,
 {
     /* Calculate the index in azimuthal direction
        where the Hill sphere starts and stops */
-    const double max_angle = std::min(M_PI_2 + M_PI_4, 2.0 * RHill / Rplanet);
-    const int i_min = get_med_azimuthal_id(angle - max_angle);
-    const int i_max = get_med_azimuthal_id(angle + max_angle) + 1;
-    std::tuple<int, int> ids(i_min, i_max);
+	const double max_angle = std::min(2.0 * M_PI, 2.0 * RHill / Rplanet);
+	const int j_min = get_med_azimuthal_id(angle - max_angle);
+	int j_max = get_med_azimuthal_id(angle + max_angle) + 1;
+	j_max = j_min + std::min(j_max - j_min, (int)NAzimuthal - 1);
+	std::tuple<int, int> ids(j_min, j_max);
     return ids;
 }
 
