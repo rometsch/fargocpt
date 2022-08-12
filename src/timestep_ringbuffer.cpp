31778c796fd097caf33de1d6e4e701d78184452f
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2022-08-12 22:23:22 +0200

    Ringbuffer Debug stuff

--- src/timestep_ringbuffer.cpp
+++ src/timestep_ringbuffer.cpp
@@ -1,4 +1,5 @@
 #include "timestep_ringbuffer.h"
+#include "logging.h"
 
 timestep_ringbuffer::timestep_ringbuffer()
 {
@@ -18,6 +19,9 @@ void timestep_ringbuffer::init(const int len, const double factor, const double 
 	}
 	m_counts[m_state] = 1;
 	m_total_times[m_state] = dt_start*10.0;
+
+	print_state();
+
 }
 
 void timestep_ringbuffer::reinit(const int len, const double factor, const double dt_start){
@@ -37,6 +41,9 @@ void timestep_ringbuffer::reinit(const int len, const double factor, const doubl
 	}
 	m_counts[m_state] = 1;
 	m_total_times[m_state] = dt_start*10.0;
+
+	print_state();
+
 }
 
 timestep_ringbuffer::~timestep_ringbuffer(){
@@ -44,8 +51,20 @@ timestep_ringbuffer::~timestep_ringbuffer(){
 	delete m_total_times;
 }
 
+void timestep_ringbuffer::print_state(){
+	logging::print(LOG_INFO "Buffer length = %d	Buffer state = %d\n", m_length, m_state);
+
+	for(int i = 0; i < m_length; ++i){
+		logging::print(LOG_INFO "Buffer %d count = %d time = %5e\n", i, m_counts[i], m_total_times[i]);
+	}
+
+}
+
 void timestep_ringbuffer::update(const double average_time, const double dt)
 {
+
+	print_state();
+
 	m_total_times[m_state] += dt;
 	m_counts[m_state]++;
 
@@ -57,8 +76,9 @@ void timestep_ringbuffer::update(const double average_time, const double dt)
 
 }
 
-
 double timestep_ringbuffer::get_mean_dt(){
+
+	print_state();
 
 	if(m_length == 0){
 		return 1.0e100; // infinity
