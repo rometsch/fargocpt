3099072f82841966ce4ddc6b1dd513bae6c84dc2
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-09-05 17:19:44 +0200

    livereload uses port 5500 by default

--- docs/run_livereload.py
+++ docs/run_livereload.py
@@ -9,16 +9,60 @@ def main():
     server.watch('_static/*', shell('make html'), delay=1)
     server.watch('_templates/*', shell('make html'), delay=1)
 
-    server.serve(root='build/html', port=get_open_port())
+    server.serve(root='build/html', port=get_next_free_port(5500))
 
-def get_open_port():
-    import socket
+import socket
+import errno
+
+def is_port_free(port):
+    """ Check whether the port is free to use on localhost.
+
+    Parameters
+    ----------
+    port: int
+        Port number to check.
+
+    Returns
+    -------
+    bool:
+        True if free, false if already in use.
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("", 0))
-    s.listen(1)
-    port = s.getsockname()[1]
-    s.close()
-    return port
 
+    try:
+        s.bind(("127.0.0.1", port))
+        rv = True
+    except socket.error as e:
+        if e.errno == errno.EADDRINUSE:
+            rv = False
+        else:
+            # something else raised the socket.error exception
+            raise
+
+    s.close()
+
+    return rv
+
+
+def get_next_free_port(port):
+    """ Find the next free port on localhost.
+    
+    Recursively test all port numbers with increments of 1 
+    starting from number 'port'.
+    
+    Parameters
+    ----------
+    port: int
+        Start looking from this port number upwards.
+        
+    Returns
+    -------
+    int
+        Next free port number.
+    """
+    if is_port_free(port):
+        return port
+    else:
+        return get_next_free_port(port + 1)
 if __name__ == '__main__':
     main()
