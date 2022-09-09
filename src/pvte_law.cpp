66c06301b6c0b47b13f0051fbe768aec40835d71
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2022-09-09 17:08:24 +0200

    Refactoring pvte_law.cpp

--- src/pvte_law.cpp
+++ src/pvte_law.cpp
@@ -455,37 +455,35 @@ void compute_gamma_mu(t_data &data)
 		logging::print_master(LOG_INFO "Lookup tables generated \n");
 	}
     */
+	const unsigned int Nr = data[t_data::DENSITY].get_size_radial();
+	const unsigned int Nphi = data[t_data::DENSITY].get_size_azimuthal();
 	#pragma omp parallel for collapse(2)
-    for (unsigned int n_radial = 0;
-	 n_radial < data[t_data::DENSITY].get_size_radial(); ++n_radial) {
-	for (unsigned int n_azimuthal = 0;
-	     n_azimuthal < data[t_data::DENSITY].get_size_azimuthal();
-	     ++n_azimuthal) {
+	for (unsigned int nr = 0; nr < Nr; ++nr) {
+	for (unsigned int naz = 0; naz < Nphi; ++naz) {
 
-	    const double sigma = data[t_data::DENSITY](n_radial, n_azimuthal);
-
-	    const double H = data[t_data::SCALE_HEIGHT](n_radial, n_azimuthal);
+		const double sigma = data[t_data::DENSITY](nr, naz);
+		const double H = data[t_data::SCALE_HEIGHT](nr, naz);
 
 	    double densityCGS, energyCGS;
 
 	    if (parameters::ShockTube > 0) {
 		densityCGS = sigma * units::density.get_cgs_factor();
-		energyCGS = data[t_data::ENERGY](n_radial, n_azimuthal) *
+		energyCGS = data[t_data::ENERGY](nr, naz) *
 			    units::energy_density.get_cgs_factor() / densityCGS;
 	    } else {
 		densityCGS =
 		    sigma / (parameters::density_factor * H) * units::density;
 
-		energyCGS = data[t_data::ENERGY](n_radial, n_azimuthal) *
+		energyCGS = data[t_data::ENERGY](nr, naz) *
 			    units::energy_density /
 			    (sigma * units::surface_density);
 	    }
 
 	    t_eosQuantities q = lookup(densityCGS, energyCGS);
 
-	    data[t_data::GAMMAEFF](n_radial, n_azimuthal) = q.geff;
-	    data[t_data::MU](n_radial, n_azimuthal) = q.mow;
-	    data[t_data::GAMMA1](n_radial, n_azimuthal) = q.g1;
+		data[t_data::GAMMAEFF](nr, naz) = q.geff;
+		data[t_data::MU](nr, naz) = q.mow;
+		data[t_data::GAMMA1](nr, naz) = q.g1;
 	}
     }
 }
