6deb214e18f1e524daf32b48db85a6dc20ba8ddf
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-09-16 11:26:25 +0200

    update circ kepler prob

--- test/circ_kepler_orbit/calc_deviation.py
+++ test/circ_kepler_orbit/calc_deviation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+import os
 
 def main():
     t, x, y = np.genfromtxt("output/monitor/bigplanet2.dat", usecols=(6,1,2), unpack=True)
@@ -12,11 +13,20 @@ def main():
 
     delta_x = x - x_theo
     diff_x = np.max(np.abs(delta_x))
-    print(f"Diff x = {diff_x}")
 
     delta_y = y - y_theo
     diff_y = np.max(np.abs(delta_y))
-    print(f"Diff y = {diff_y}")
+
+    test_name = "Circular Kepler Problem"
+
+    threshold = 1e-11
+    if max(diff_x, diff_y) > threshold:
+        print(f"FAIL: {test_name} at {os.getcwd()}")
+        print(f"Diff x = {diff_x}, Diff y = {diff_y}, threshold = {threshold}")
+    else:
+        print(f"SUCCESS: {test_name}")
+
+
 
 if __name__=="__main__":
     main()
