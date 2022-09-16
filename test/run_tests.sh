d350eb52e3d3090d53dcda7267096be6442b5d43
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-09-16 11:41:05 +0200

    build code for tests if needed

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,6 +3,13 @@
 
 FILEDIR="$(dirname $(realpath $0))"
 cd "$FILEDIR"
+
+if [[ ! -e "../fargo" ]]
+then
+    echo "Building fargo..."
+    make -C ../src -j > ../make.log
+fi
+
 for DIR in $(find . -mindepth 1 -maxdepth 1 -type d)
 do
     if [[ -e "$DIR/run_auto_test.sh" ]];
