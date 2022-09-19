44c677302f086bc43fc4d92f3fefdc5005a0808d
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-09-19 16:21:55 +0200

    make shocktube test success/fail

--- test/shockTube/shockTube.py
+++ test/shockTube/shockTube.py
@@ -3,28 +3,80 @@ Test for Super-Time stepping from D'Angelo et al. 2003 THERMOHYDRODYNAMICS OF CI
 """
 import subprocess
 import os
+import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy import interpolate, integrate
+
+quants = ["gasvrad", "gasdens", "gasTemperature", "gasenergy"]
+
+acceptable_diff = {
+    "gasvrad" : 0.015, 
+    "gasdens" : 0.0073,
+    "gasTemperature" : 0.016, 
+    "gasenergy" : 0.014
+}
+
+test_cases = {
+    "SN" : {
+        "outdir" : 'output/SN/',
+        "color" : "red",
+        "ls" : "--"
+    },
+    "TW" : {
+        "outdir" : 'output/TW/',
+        "color" : "blue",
+        "ls" : "--"
+    },
+    "SN STS" : {
+        "outdir" : 'output/STS/',
+        "color" : "gold",
+        "ls" : ":"
+    },
+    "TW LF" : {
+        "outdir" : 'output/TW_LF/',
+        "color" : "green",
+        "ls" : "-."
+    },
+    "SN LF" : {
+        "outdir" : 'output/SN_LF/',
+        "color" : "orange",
+        "ls" : ":"
+    }
+}
 
 def compile_fargo(fargo_path):
     wd = os.getcwd()
     os.chdir(fargo_path)
 
-    subprocess.run(['make', '-j', '-C' 'src/'])
+    with open("make.log", "w") as logfile:
+        subprocess.run(['make', '-j', '-C' 'src/'], stdout=logfile)
     os.chdir(wd)
 
 
-def run(fargo_path, par_file):
-    # wd = os.getcwd()
-    # os.chdir(fargo_path)
+def run(par_file):
+    logfilename = os.path.basename(par_file).replace(".yml",".log")
+    with open(logfilename, "w") as logfile:
+        subprocess.call('../../fargo start ' + par_file, shell=True, stdout=logfile)
 
-    subprocess.call('../../fargo start ' + par_file, shell=True)
-    # os.chdir(wd)
+def get_analytic_spl(quant):
+    analytic_data = np.loadtxt("analytic_shock.dat", skiprows=2)
+    x = analytic_data[:,1]
+    i = None
+    for n, s in enumerate(quants):
+        if s == quant:
+            i = n
+    if i is None:
+        raise ValueError(f"{quant} is not a valid quantity")
+    y = analytic_data[:,(i+2)]
+    if quants[i] == 'gasenergy':
+      y = analytic_data[:,4]*analytic_data[:,3]/(1.4-1)
+      
+    s = interpolate.InterpolatedUnivariateSpline(x, y)
 
-
-quants = ["gasvrad", "gasdens", "gasTemperature", "gasenergy"]
+    return s
 
 def analytic(axs):
     analytic_data = np.loadtxt("analytic_shock.dat", skiprows=2)
@@ -39,14 +91,11 @@ def analytic(axs):
         ax.plot(x, y, '-k', label='Analytic', lw=2)
 
 
-def test(out, label, color, dt, ls="--"):
+def plot_output(out, label, color, Nsnap, axs, ls="--"):
 
     r12 = np.loadtxt(out + "used_rad.dat", skiprows=0)
     r1 = 0.5*(r12[1:] + r12[:-1])-r12[0]
-    rmax = np.max(r1)
-    rmin = np.min(r1)
-
-    file_name = f"{out}/snapshots/{dt}/Sigma.dat"
+    file_name = f"{out}/snapshots/{Nsnap}/Sigma.dat"
     data = np.fromfile(file_name)
     N = len(data)
     nr = len(r1)
@@ -57,7 +106,7 @@ def test(out, label, color, dt, ls="--"):
 
         name = quants[i].replace("gas", "")
         name = name.replace("dens", "Sigma")
-        file_name = f"{out}/snapshots/{dt}/{name}.dat"
+        file_name = f"{out}/snapshots/{Nsnap}/{name}.dat"
         data = np.fromfile(file_name)
         if name == 'vrad':
             data = data.reshape((nr+1, nphi))
@@ -70,40 +119,100 @@ def test(out, label, color, dt, ls="--"):
         ax.plot(r1, data, ls=ls, color=color, label=label, lw=2.5)
 
 
+def diff_to_analytic(out, quant, Nsnap):
+
+    r12 = np.loadtxt(out + "used_rad.dat", skiprows=0)
+    r1 = 0.5*(r12[1:] + r12[:-1])-r12[0]
+    file_name = f"{out}/snapshots/{Nsnap}/Sigma.dat"
+    data = np.fromfile(file_name)
+    N = len(data)
+    nr = len(r1)
+    nphi = int(N/nr)
 
 
-compile_fargo('../../')
-run('../../', 'setups/shocktube_SN.yml')
-run('../../', 'setups/shocktube_TW.yml')
-run('../../', 'setups/shocktube_STS.yml')
-run('../../', 'setups/shocktube_TW_LF.yml')
-run('../../', 'setups/shocktube_SN_LF.yml')
+    name = quant.replace("gas", "")
+    name = name.replace("dens", "Sigma")
+    file_name = f"{out}/snapshots/{Nsnap}/{name}.dat"
+    data = np.fromfile(file_name)
+    if name == 'vrad':
+        data = data.reshape((nr+1, nphi))
+        data = np.mean(data, 1)
+        data = 0.5*(data[1:] + data[:-1])
+    else:
+        data = data.reshape((nr, nphi))
+        data = np.mean(data, 1)
 
-dt = 228
-fig, axs = plt.subplots(2,2,figsize=(8,4))
-axs = np.ravel(axs)
-
-analytic(axs)
-test('output/SN/', 'SN', 'red', dt)
-test('output/TW/', 'TW', 'blue', dt)
-test('output/STS/', 'SN STS', 'gold', dt, ls=":")
-test('output/TW_LF/', 'TW LF', 'green', dt, ls="-.")
-test('output/SN_LF/', 'SN LF', 'orange', dt, ls=":")
-
-
-for i in range(len(quants)):
-    ax = axs[i]
-    ax.axis('auto')
-    ax.set_title(quants[i], color='black', y = 1.06)
-    if quants[i] == 'gasdens':
-        ax.legend(loc='upper right')
-    if quants[i] == 'gasenergy':
-        ax.legend(loc='upper right')
-    if quants[i] == 'gasvrad':
-        ax.legend(loc='upper left')
-    if quants[i] == 'gasTemperature':
-        ax.legend(loc='upper left')
+    # restrict to 0 to 1
+    inds = np.logical_and(r1 >= 0, r1 <=1)
+    r1 = r1[inds]
+    data = data[inds] 
 
 
+    spl = get_analytic_spl(quant)
+    analytic = spl(r1)
 
-plt.show()
+    delta = np.abs(data - analytic)
+    return integrate.simpson(delta, r1)
+
+
+def visualize(Nsnapshot):
+    fig, axs = plt.subplots(2,2,figsize=(8,4))
+    axs = np.ravel(axs)
+
+    analytic(axs)
+    for key, val in test_cases.items():
+        plot_output(val["outdir"], 
+                    key, 
+                    val["color"], 
+                    Nsnapshot, 
+                    axs, 
+                    ls=val["ls"])
+
+    for i in range(len(quants)):
+        ax = axs[i]
+        ax.axis('auto')
+        ax.set_title(quants[i], color='black', y = 1.06)
+        if quants[i] == 'gasdens':
+            ax.legend(loc='upper right')
+        if quants[i] == 'gasenergy':
+            ax.legend(loc='upper right')
+        if quants[i] == 'gasvrad':
+            ax.legend(loc='upper left')
+        if quants[i] == 'gasTemperature':
+            ax.legend(loc='upper left')
+    plt.show()
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--viz", action="store_true", help="Plot the results.")
+    opt = parser.parse_args()
+    
+    compile_fargo('../../')
+    run('setups/shocktube_SN.yml')
+    run('setups/shocktube_TW.yml')
+    run('setups/shocktube_STS.yml')
+    run('setups/shocktube_TW_LF.yml')
+    run('setups/shocktube_SN_LF.yml')
+    
+    success = True
+    with open("diffs.log", "w") as logfile:
+        for key, val in test_cases.items():
+            outdir = val["outdir"]
+            for quant in quants:
+                diff = diff_to_analytic(outdir, quant, 228)
+                is_smaller = diff < acceptable_diff[quant]
+                if not is_smaller:
+                    success = False
+                print("SUCCESS" if is_smaller else "FAIL", "|",
+                      key, "|", quant, "|", diff, file=logfile)
+    if success:
+        print("SUCCESS: shocktube test")
+    else:
+        print("FAIL: shocktube test, threshold of integrated diff exceeded")
+    
+    if opt.viz:
+       visualize(228)
+
+if __name__=="__main__":
+    main()
