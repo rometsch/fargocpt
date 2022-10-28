ad93b1536e7bcf0e5f9aef89020cefde98fb2aa9
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2022-10-28 13:11:54 +0200

    Updated gasMassflow test

--- Tools/read_data/read_gasMassFlow1D.py
+++ Tools/read_data/read_gasMassFlow1D.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 class gasMassFlow1D:
     def __init__(self, output_folder_path, print_unit="solMass/yr"):
         self.output_folder_path = output_folder_path
-        with open(self.output_folder_path + "gasMassFlow1D.info") as f:
+        with open(self.output_folder_path + "MassFlow1D.info") as f:
+            header = f.readline()
+            header = f.readline()
             header = f.readline()
             header = f.readline()
 
@@ -17,25 +19,19 @@ class gasMassFlow1D:
             self.unit = re.search("unit = ([^,]*)", header).groups()[0].strip()
             self.print_unit = print_unit
 
-        self.DT = np.loadtxt(self.output_folder_path + "Quantities.dat", usecols=1, dtype=np.int)
-    def read(self):
-        with open(self.output_folder_path + "gasMassFlow1D.dat", "rb") as f:
+        self.DT = np.loadtxt(self.output_folder_path + "/monitor/Quantities.dat", usecols=1, dtype=np.int)
+    def read(self, dt):
+        with open(self.output_folder_path + f"/snapshots/{dt}/MassFlow1D.dat", "rb") as f:
             Nr = self.Nr
-            data = np.fromfile(f)
-            rs = data[:Nr]
-            data = data[Nr:]
-            data = data
-            num_outputs = len(data)/Nr
-            if num_outputs%1 != 0:
-                raise ValueError("Output data length ({}) is not a multiple of Nr = {}".format(len(data), Nr))
-            num_outputs = int(num_outputs)
-            data = data.reshape( int(num_outputs), Nr)
+            data = np.fromfile(f).reshape(Nr, 4)
+            rs = data[:,0]
+            data = data[:,1]
             data = data*u.Unit(self.unit).to(self.print_unit)
-            return rs, self.DT, data, num_outputs
+            return rs, self.DT, data
 
 
 if __name__ == "__main__":
-    mass_flow_reader = gasMassFlow1D("/home/jordan/UnitTest/fargocpt/Tools/test_problems/gasMassFlow1D/out/")
+    mass_flow_reader = gasMassFlow1D("/home/jordan/develop/fargocpt/test/gasMassFlow1D/out/")
 
-    rs, DT, data, Noutputs = mass_flow_reader.read()
-    print(data[0])
+    rs, DT, data = mass_flow_reader.read(0)
+    print(data)
