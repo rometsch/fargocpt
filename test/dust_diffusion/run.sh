f99db646bfa203498a8149e13e5798f22c7b4a9e
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-11-25 16:32:38 +0100

    update

--- test/dust_diffusion/run.sh
+++ test/dust_diffusion/run.sh
@@ -5,10 +5,10 @@ if [[ $? > 0 ]]; then
     exit $?
 fi
 
-oldpid="$(ps ax | grep fargo | grep mpirun | cut -d' ' -f1)"
-if [[ -n "$oldpid" ]]; then
-    kill $oldpid
-fi
+# oldpid="$(ps ax | grep fargo | grep mpirun | cut -d' ' -f1)"
+# if [[ -n "$oldpid" ]]; then
+#     kill $oldpid
+# fi
 
 
 LOGFILE="output/simulation.log"
@@ -16,11 +16,14 @@ LOGFILE="output/simulation.log"
 rm -rf output/dust_diffusion
 mkdir -p output
 
-if [[ -n "$(command -v squeue)" ]]
-then
-    sbatch -n 6 -o "$LOGFILE" slurm_start.sh
-    touch $LOGFILE
-    tail -f $LOGFILE
-else
-    ../../fargo start setup/simple_model.yml
-fi
+# if [[ -n "$(command -v squeue)" ]]
+# then
+#     sbatch -n 6 -o "$LOGFILE" slurm_start.sh
+#     touch $LOGFILE
+#     tail -f $LOGFILE
+# else
+#     ../../fargo start setup/simple_model.yml
+# fi
+export N_MPI=1
+export OMP_NUM_THREADS=1
+numactl mpirun -n $N_MPI --map-by NUMA:PE=$OMP_NUM_THREADS ../../fargo -v start setup/Charnoz2011_fast.yml
