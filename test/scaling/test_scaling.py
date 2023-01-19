b4aacd7538b684ba681b6d8e0c92d779893de81a
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-01-19 20:40:41 +0100

    update scaling script to new timestep logging output

--- test/scaling/test_scaling.py
+++ test/scaling/test_scaling.py
@@ -7,9 +7,9 @@ import numpy as np
 import re
 
 Nproc_min = 1
-Nproc_max = 2
+Nproc_max = 1
 Nthread_min = 1
-Nthread_max = 6
+Nthread_max = 2
 for nproc in range(Nproc_min, Nproc_max+1):
     for nthread in range(Nthread_min, Nthread_max+1):
 
@@ -17,14 +17,5 @@ for nproc in range(Nproc_min, Nproc_max+1):
         with open(filename, "w") as logfile:
             run_fargo(nproc,nthread,["start", "testconfig.yml"], stdout=logfile, stderr=logfile)
 
-
-        with open(filename, "r") as logfile:
-            ptrn = re.compile(r"Time per Step:\s([0-9\.]+)\smilliseconds")
-            for line in logfile:
-                m = re.search(ptrn, line)
-                if m is not None:
-                    ms_per_step = m.groups()[0]
-
-        # num_steps, time = np.genfromtxt("output/out/monitor/timestepLogging.dat", usecols=(4,3))[1:].T
-        # time_per_hydrostep = (time[-1] - time[0])/np.sum(num_steps)
-        print(nproc, nthread, ms_per_step)
+        time_per_step_ms = np.genfromtxt("output/out/monitor/timestepLogging.dat", usecols=6)[1:]
+        print(nproc, nthread, np.median(time_per_step_ms))
