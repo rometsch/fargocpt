ba92bd7f702b139943466936ea212c6a8253de31
Thomas Rometsch <thomas.rometsch@uni-tuebingen.de>
2023-01-30 19:37:19 +0100

    update docker scripts

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 3 ]; then
+    echo "usage: $0 {start/auto} {inputfile} {simulationdir}"
+    exit 1
+fi
+
 COMMAND=$1
 INPUTFILE="$(realpath $2)"
-OUTPUTDIR="$(realpath $3)"
+SIMDIR="$(realpath $3)"
+
+if [ ! -e "$INPUTFILE" ]; then
+    echo "Input file $INPUTFILE does not exist!"
+    exit 1
+fi
+
+if [ ! -d "$SIMDIR" ]; then
+    echo "Simulation directory $SIMDIR does not exist!"
+    exit 1
+fi
 
 sudo docker run \
     -e LOCAL_USER_ID=$UID \
-    -v $OUTPUTDIR:/output \
-    -v $INPUTFILE:/app/setup.yml \
+    -v $SIMDIR:/simulation \
+    -v $INPUTFILE:/simulation/setup.yml \
     fargocpt \
     fargocpt -np 1 \
     $COMMAND \
