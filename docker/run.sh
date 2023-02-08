22c832345d505fba56dc088728acf8df60893702
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-02-08 20:42:51 +0100

    add alpine based docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,29 +1,21 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-    echo "usage: $0 {start/auto} {inputfile} {simulationdir}"
+if [ $# -le 2 ]; then
+    echo "usage: $0 {simulationdir} {fargo args}"
     exit 1
 fi
 
-COMMAND=$1
-INPUTFILE="$(realpath $2)"
-SIMDIR="$(realpath $3)"
-
-if [ ! -e "$INPUTFILE" ]; then
-    echo "Input file $INPUTFILE does not exist!"
-    exit 1
-fi
+SIMDIR="$(realpath $1)"
 
 if [ ! -d "$SIMDIR" ]; then
     echo "Simulation directory $SIMDIR does not exist!"
     exit 1
 fi
 
-sudo docker run \
+shift 1
+
+docker run \
     -e LOCAL_USER_ID=$UID \
-    -v $SIMDIR:/simulation \
-    -v $INPUTFILE:/simulation/setup.yml \
-    fargocpt \
-    fargocpt -np 1 \
-    $COMMAND \
-    setup.yml
+    -v $SIMDIR:/project \
+    fargocpt-dev \
+    fargocpt -np 1 "$@"
