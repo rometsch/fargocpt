da34595aa387cb6336113063858f296f923837a9
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2023-05-24 17:12:28 +0200

    Small updates to Temperature test & file that created paper plot.

--- test/TemperatureTest/paper_plot.py
+++ test/TemperatureTest/paper_plot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# %%
 import subprocess
 import os
 
@@ -30,9 +31,9 @@ def test(out1, dt):
     r1 = data1[::4]
     quant1 = data1[1::4]
 
-    fig = plt.figure()
-    ax = fig.add_subplot(121)
-    ax2 = fig.add_subplot(122)
+    fig, axs = plt.subplots(1,1, figsize=(6,4.5))
+    ax2 = axs
+    ax = ax2.twinx()
 
     rmax = np.max(r1)
     rmin = np.min(r1)
@@ -54,24 +55,28 @@ def test(out1, dt):
     G = 6.674e-8 # dyne cm^2/g^2
     omega_k = np.sqrt(G*m0*(r*l0)**(-3))
     ax.axis('auto')
-    ax.set_title('Temperature', color='black', y = 1.06)
+    # ax.set_title('Temperature', color='black', y = 1.06)
     T = np.sqrt(27/128*kappa*nu/sigma) * dens * omega_k
-    ax.plot(r, T, '--k', label='Theory', lw=2.5)
+    T = 104 * (5/r1.flatten())**2
+    ax.plot(r, T, '-k', label='T theory', lw=5)
+    ax.set_ylabel('log10 T [K]')
 
     vmin = min(vmin, np.min(T))
     vmax = max(vmax, np.max(T))
 
-    ax.plot(r1.flatten(), quant1.flatten() * T0, '.r', label='Code', lw=2.5)
+    ax.plot(r1.flatten(), quant1.flatten() * T0, '.', color='C0', label='T code', lw=2.5)
 
     file_name = out1 + f"snapshots/{dt}/Sigma1D.dat"
     data_dens = np.fromfile(file_name)
     quant2 = data_dens[1::4] * Sigma0
 
     ax2.axis('auto')
-    ax2.set_title('Density', color='black', y = 1.06)
+    # ax2.set_title('Density', color='black', y = 1.06)
     dens = 300*np.sqrt(5/r1.flatten())
-    ax2.plot(r1.flatten(), dens, '--k', label='Theory', lw=2.5)
-    ax2.plot(r1.flatten(), quant2, '.r', label='Code', lw=2.5)
+    ax2.plot(r1.flatten(), dens, '-k', label='$\Sigma$ theory', lw=5)
+    ax2.plot(r1.flatten(), quant2, '.', color='C1', label='$\Sigma$ code', lw=2.5)
+    ax2.set_ylabel('log10 $\Sigma$ [g/cm²]')
+    ax.set_xlabel('log10 R [au]')
 
     vmin2 = np.min(quant2)
     vmax2 = np.max(quant2)
@@ -81,27 +86,59 @@ def test(out1, dt):
 
 
 
+    ax2.legend(loc='lower left')
     ax.legend(loc='upper right')
-    ax2.legend(loc='upper right')
 
 
     log = True
-    ax.set_xlim(rmin,rmax)
-    ax.set_ylim(vmin,vmax)
+    ax.set_xlim(rmin*0.95,rmax*1.05)
+    ax.set_ylim(vmin*800,vmax*1.2)
     if log:
         ax.set_yscale("log", nonpositive='clip')
         ax.set_xscale("log")
 
-    ax2.set_xlim(rmin,rmax)
-    ax2.set_ylim(vmin2,vmax2)
+    ax2.set_ylim(vmin2*0.95,vmax2*1.2)
     if log:
         ax2.set_yscale("log", nonpositive='clip')
         ax2.set_xscale("log")
 
 
 
+    plt.savefig('TemperatureTest.pdf', dpi=300, bbox_inches='tight')
     plt.show()
 
-compile_fargo('../../')
-run('../../', 'test/TemperatureTest/angelo.yml')
+#compile_fargo('../../')
+#run('../../', 'test/TemperatureTest/angelo.yml')
 test('../../angelo/', 10)
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
