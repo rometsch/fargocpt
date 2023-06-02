cb5a09fe5cc6e16e86a3cde0936e3360bd0ec05e
Lucas Jordan <lucas.jordan@uni-tuebingen.de>
2023-06-02 16:53:08 +0200

    Changed viscous accretion function from 1 - r/R to 1

--- src/accretion.cpp
+++ src/accretion.cpp
@@ -363,9 +363,9 @@ static bool AccreteOntoSinglePlanetViscous(t_data &data, t_planet &planet,
     //const double area = 2.0 * M_PI * ((1.0/2.0 * std::pow(dist_max, 2) - 1.0/3.0 * std::pow(dist_max, 2))
     //                        -(1.0/2.0 * std::pow(RMIN, 2)     - 1.0/3.0 * std::pow(RMIN, 3)/dist_max));
 
-    // f = 1 - sqrt(r/R)
-    const double area1 = M_PI * std::pow(dist_max, 2) * (1.0 - 4.0/5.0);
-    const double area2 = M_PI * std::pow(RMIN, 2) * (1.0 - 4.0/5.0 * std::sqrt(RMIN/dist_max));
+    // f = 1
+    const double area1 = M_PI * std::pow(dist_max, 2);
+    const double area2 = M_PI * std::pow(RMIN, 2);
     const double area = area1 - area2;
 
     f_const = 1.0 / area;
@@ -373,8 +373,8 @@ static bool AccreteOntoSinglePlanetViscous(t_data &data, t_planet &planet,
     // f = 1 - r/R
     // f_const = 3.0 / M_PI / std::pow(dist_max, 2);
 
-    // f = 1 - sqrt(r/R)
-    const double area = M_PI * std::pow(dist_max, 2) * (1.0 - 1.0/5.0);
+    // f = 1
+    const double area = M_PI * std::pow(dist_max, 2);
     f_const = 1.0 / area;
     }
 
@@ -413,7 +413,7 @@ static bool AccreteOntoSinglePlanetViscous(t_data &data, t_planet &planet,
         // we have to remove it to compute the correct change in Sigma
         // and then add it again for computing the accreted mass.
         const double spread =
-            f_const * (1.0 - std::sqrt(distance / dist_max));
+            f_const;// * (1.0 - std::sqrt(distance / dist_max));
         //const double spread =
            //f_const * (1.0 - distance / dist_max);
 
