ea72b028608b9443b00e294852b2c13fa202ff8e
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-06-27 08:25:27 +0200

    update 1d fld test for new code

--- test/FLD1D/run_auto_test.sh
+++ test/FLD1D/run_auto_test.sh
@@ -4,10 +4,10 @@ FILEDIR="$(dirname $(realpath $0))"
 cd $FILEDIR
 
 # we need to modify the build to enable a constant flux limiter
-./modify_build.sh > modify_build.log
+# ./modify_build.sh > modify_build.log
 
 ../../run_fargo -nt 2 -np 1 start setup.yml 1> out.log 2>err.log
 ./calc_deviation.py
 
 # revert the changes to the code
-./revert_build.sh > revert_build.log
+# ./revert_build.sh > revert_build.log
