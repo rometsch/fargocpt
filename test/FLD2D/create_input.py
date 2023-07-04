bd4adbae0e8ec63a1575313cf28f63b38be3e88e
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-07-04 11:20:12 +0200

    fix and add plots to exact matrix solver script

--- test/FLD2D/create_input.py
+++ test/FLD2D/create_input.py
@@ -17,6 +17,12 @@ with open("setup.yml", "r") as infile:
 ri = np.linspace(rmin, rmax, Nrad+1)
 phii = np.linspace(0, 2*np.pi, Naz+1)
 Ri, Phii = np.meshgrid(ri, phii, indexing="ij")
+rc = 2/3*(ri[1:]**2/(ri[1:]+ri[:-1])) + ri[:-1] # approx center in polar coords
+phic = 0.5*(phii[1:]+phii[:-1])
+
+Rc, Phic = np.meshgrid(rc, phic, indexing="ij")
+Xc = Rc*np.cos(Phic)
+Yc = Rc*np.sin(Phic)
 
 dphi = phii[1] - phii[0]
 dr = ri[1:] - ri[:-1]
@@ -29,12 +35,29 @@ nphi = 0
 
 # print(dr[nr])
 
+c = 1e10
+rho = 1
+kappa = 1
+K = 1/3*c/(rho*kappa)
+
+xcell = Xc[nr, nphi]
+ycell = Yc[nr, nphi]
+DX = Xc - xcell
+DY = Yc - ycell
+Dist = np.sqrt(DX**2 + DY**2)
+
+def analytical_solution(r, t, E0, K):
+    return 3*E0/(4*np.pi*t*K)*np.exp(-r**2/(4*K*t))
+
+t0 = 1e-10
+
 energy0 = 1e10 / A[nr, nphi]
 # energymin = 1e-10*energy0
-energymin = 0
-energy = np.ones((Nrad, Naz), dtype=np.float64)*energymin
+# energymin = 0
+# energy = np.ones((Nrad, Naz), dtype=np.float64)*energymin
 
-energy[nr, nphi] = energy0
+initial_condition = analytical_solution(Dist,t0, energy0, K)
+energy = initial_condition
 
 # save this energy array to file
 energy.tofile("output/out/snapshots/0/energy.dat")
--- test/FLD2D/solve.py
+++ test/FLD2D/solve.py
@@ -42,30 +42,56 @@ A(k, (k-1)%N) = dij
 A(k, (k+1)%N) = eij
 """
 
-Xold = np.fromfile(outdir + "/snapshots/0/energy.dat", dtype=np.float64)
+internal_energy = np.fromfile(outdir + "/snapshots/0/energy.dat", dtype=np.float64)
+# convert internal energy to energy density
+rho = 1 # in cgs
+cV = 8.76e7
+Trad = internal_energy/rho/cV
+aR = 1.89e-15
+Erad = aR*Trad**4
+
+Xold = Erad
+
 a = np.fromfile(outdir + "/snapshots/1/A.dat", dtype=np.float64)
 b = np.fromfile(outdir + "/snapshots/1/B.dat", dtype=np.float64)
 c = np.fromfile(outdir + "/snapshots/1/C.dat", dtype=np.float64)
 d = np.fromfile(outdir + "/snapshots/1/D.dat", dtype=np.float64)
 e = np.fromfile(outdir + "/snapshots/1/E.dat", dtype=np.float64)
 
-b = b.reshape(Nr, Naz)
-b[0,:] = b[1,:]
-b[-1,:] = b[-2,:]
-b = b.flatten()
+# b = b.reshape(Nr, Naz)
+# b[0,:] = b[1,:]
+# b[-1,:] = b[-2,:]
+# b = b.flatten()
 
 N = Nr*Naz
 
 A = np.zeros((N,N))
 
+
 for k in range(N):
-    A[k,k] = b[k]
+    if b[k] == 0:
+        A[k,k] = 1
+    else:
+        A[k,k] = b[k]
     A[k, (k-Naz)%N] = a[k]
     A[k, (k+Naz)%N] = c[k]
-    A[k, (k-1)%N] = d[k]
-    A[k, (k+1)%N] = e[k]
+    if k%Naz == 0:
+        A[k, (k+Naz-1)%N] = d[k]
+    else:
+        A[k, (k-1)%N] = d[k]
+    if (k+1)%Naz == 0:
+        A[k, (k-Naz+1)%N] = e[k]
+    else:
+        A[k, (k+1)%N] = e[k]
+
+for k in range(N):
     if A[k,k] == 0:
-        print(f"Diagonal element is zero for k={k}")
+        print(f"Zero diagonal element for k = {k}")
+
+
+# print(np.linalg.norm(A, ord=1))
+# exit()
+
 
 Xnew = np.linalg.solve(A, Xold)
 print(Nr*Naz,len(Xnew))
@@ -77,12 +103,19 @@ Plotting the result
 import matplotlib.pyplot as plt
 import matplotlib.colors as mplcolors
 
+# fig, ax = plt.subplots()
+# ax.imshow(np.log10(np.abs(A)),cmap="magma")
+
+# for n in range(Nr):
+#     ax.axhline(n*Naz-0.5,color="black", alpha=0.4, lw=0.1)
+#     ax.axvline(n*Naz-0.5,color="black", alpha=0.4, lw=0.1)
+
+# plt.show()
+# exit()
 
 fig, ax = plt.subplots(dpi=150)
 
 Z = Xnew.reshape(Nr, Naz)
-
-print(X.shape)
 
 vmin = np.min(Z)
 vmax = np.max(Z)
@@ -102,9 +135,146 @@ pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
 ax.set_aspect("equal")
 
 cbar = fig.colorbar(pcm, ax=ax)
-# cbar.set_label(f"{name} [{dataunit}]")
+ax.set_title("Exact solution")
 
 # ax.set_xlabel(f"x [{lengthunit}]")
 # ax.set_ylabel(f"y [{lengthunit}]")
+
+
+"""
+Plot exact solution symmetry
+"""
+fig, ax = plt.subplots(dpi=150)
+
+Z = Xnew.reshape(Nr, Naz)
+Z = Z - np.roll(Z, -1, axis=1)[:,::-1]
+
+
+vmin = np.min(Z)
+vmax = np.max(Z)
+
+logplot = False
+cmap = "magma"
+if logplot:
+    vmin = 1e-20 + np.min(np.abs(Z))
+    vmax = 1e-20 + np.max(np.abs(Z))
+    print(vmin, vmax)
+    norm = mplcolors.LogNorm(vmin=vmin, vmax=vmax)
+    Z = np.abs(Z)
+else:
+    norm = mplcolors.Normalize(vmin=vmin, vmax=vmax)
+
+pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
+ax.set_aspect("equal")
+
+cbar = fig.colorbar(pcm, ax=ax)
+ax.set_title("Exact solution symmetry")
+
+"""
+Plot SOR solution
+"""
+
+fig, ax = plt.subplots(dpi=150)
+
+Z = np.fromfile(outdir + "/snapshots/1/Erad.dat", dtype=np.float64).reshape(Nr, Naz)
+
+vmin = np.min(Z)
+vmax = np.max(Z)
+
+logplot = False
+cmap = "magma"
+if logplot:
+    vmin = 1e-20 + np.min(np.abs(Z))
+    vmax = 1e-20 + np.max(np.abs(Z))
+    print(vmin, vmax)
+    norm = mplcolors.LogNorm(vmin=vmin, vmax=vmax)
+    Z = np.abs(Z)
+else:
+    norm = mplcolors.Normalize(vmin=vmin, vmax=vmax)
+
+pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
+ax.set_aspect("equal")
+
+cbar = fig.colorbar(pcm, ax=ax)
+ax.set_title("SOR solution")
+
+"""
+Plot SOR solution symmetry
+"""
+
+fig, ax = plt.subplots(dpi=150)
+
+Z = np.fromfile(outdir + "/snapshots/1/Erad.dat", dtype=np.float64).reshape(Nr, Naz)
+Z = Z - np.roll(Z, -1, axis=1)[:,::-1]
+
+vmin = np.min(Z)
+vmax = np.max(Z)
+
+logplot = False
+cmap = "magma"
+if logplot:
+    vmin = 1e-20 + np.min(np.abs(Z))
+    vmax = 1e-20 + np.max(np.abs(Z))
+    print(vmin, vmax)
+    norm = mplcolors.LogNorm(vmin=vmin, vmax=vmax)
+    Z = np.abs(Z)
+else:
+    norm = mplcolors.Normalize(vmin=vmin, vmax=vmax)
+
+pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
+ax.set_aspect("equal")
+
+cbar = fig.colorbar(pcm, ax=ax)
+ax.set_title("SOR solution symmetry")
+
+"""
+Plot initial condition
+"""
+
+fig, ax = plt.subplots(dpi=150)
+
+Z = Xold.reshape(Nr, Naz)
+
+vmin = np.min(Z)
+vmax = np.max(Z)
+
+logplot = False
+cmap = "magma"
+if logplot:
+    vmin = 1e-20 + np.min(np.abs(Z))
+    vmax = 1e-20 + np.max(np.abs(Z))
+    print(vmin, vmax)
+    norm = mplcolors.LogNorm(vmin=vmin, vmax=vmax)
+    Z = np.abs(Z)
+else:
+    norm = mplcolors.Normalize(vmin=vmin, vmax=vmax)
+
+pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
+ax.set_aspect("equal")
+
+cbar = fig.colorbar(pcm, ax=ax)
+ax.set_title("initial condition")
+
+"""
+Plot difference
+"""
+
+fig, ax = plt.subplots(dpi=150)
+
+Z = np.fromfile(outdir + "/snapshots/1/Erad.dat", dtype=np.float64).reshape(Nr, Naz) - Xnew.reshape(Nr, Naz)
+
+vmin = np.min(Z)
+vmax = np.max(Z)
+
+logplot = False
+cmap = "bwr"
+norm = mplcolors.SymLogNorm(vmin=-vmax, vmax=vmax, linthresh = 1e-2*vmax)
+
+pcm = ax.pcolormesh(X,Y,Z, norm=norm, cmap=cmap)
+ax.set_aspect("equal")
+
+cbar = fig.colorbar(pcm, ax=ax)
+ax.set_title("difference SOR - exact")
+
 
 plt.show()
