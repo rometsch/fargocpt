da6eb3f694f8e6961d7322a80980ee663d6740b7
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-08-16 10:47:46 +0200

    add a script to run callgrind

---
+++ Tools/profiling_callgrind.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "This tool runs a the callgrind tool of valgrind."
+echo "The simulation will run much slower than usual (expect factor 20+ slower)."
+echo "You need to call the binary directly without the wrapper."
+
+FILEDIR="$(realpath $(dirname $0))"
+FARGOCMD="$FILEDIR/../bin/fargocpt"
+
+# Check if at least two arguments are provided
+if [ $# -lt 3 ]; then
+  echo "usage: $(basename $0) Nthread <fargo command>"
+  exit 1
+fi
+
+# Assign the first two arguments to variables
+Nthread="$1"
+
+# Shift the first two arguments
+shift 1
+
+# Print the first two arguments
+echo "Using $Nproc MPI processes and $Nthread OMP threads"
+
+export OMP_NUM_THREADS="$Nthread"
+valgrind --tool=callgrind $Nproc "$FARGOCMD" "$@"
+
+echo "Now view the results using e.g. 'kcachegrind callgrind.out.<PID>'
