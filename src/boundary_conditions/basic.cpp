83fe3ffc1b0b9b159f260782d105521027c1eb4e
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-09-22 08:40:08 +0200

    move reflecting bc to separate file

--- src/boundary_conditions/basic.cpp
+++ src/boundary_conditions/basic.cpp
@@ -17,61 +17,6 @@
 namespace boundary_conditions
 {
 
-/**
-	inner reflecting boundary condition
-*/
-void reflecting_boundary_inner(t_data &data)
-{
-    if (CPU_Rank != 0) {
-		return;
-	}
-
-	t_polargrid &sig = data[t_data::SIGMA];
-	t_polargrid &e = data[t_data::ENERGY];
-	t_polargrid &vr = data[t_data::V_RADIAL];
-
-	const unsigned int Naz = sig.get_max_azimuthal();
-
-	#pragma omp parallel for
-	for (unsigned int naz = 0; naz <= Naz; ++naz) {
-	    // copy first ring into ghost ring
-	    sig(0, naz) = sig(1, naz);
-	    e(0, naz) = e(1, naz);
-
-	    vr(1, naz) = 0.0;
-	    vr(0, naz) = -vr(2, naz);
-	}
-}
-
-/**
-	outer reflecting boundary condition
-*/
-void reflecting_boundary_outer(t_data &data)
-{
-    if (CPU_Rank != CPU_Highest) {
-		return;
-	}
-
-	t_polargrid &sig = data[t_data::SIGMA];
-	t_polargrid &e = data[t_data::ENERGY];
-	t_polargrid &vr = data[t_data::V_RADIAL];
-
-	const unsigned int Nrad = sig.get_max_radial();
-	const unsigned int Nrad_v = vr.get_max_radial();
-	const unsigned int Naz = sig.get_max_azimuthal();
-
-
-	#pragma omp parallel for
-	for (unsigned int naz = 0; naz <= Naz; ++naz) {
-	    // copy last ring into ghost ring
-	    sig(Nrad, naz) = sig(Nrad - 1, naz);
-	    e(Nrad, naz) = e(Nrad - 1, naz);
-
-	    vr(Nrad_v - 1, naz) = 0.0;
-	    vr(Nrad_v, naz) = -vr(Nrad_v - 2, naz);
-	}
-}
-
 void viscous_outflow_boundary_inner(t_data &data)
 {
     if (CPU_Rank != 0) {
