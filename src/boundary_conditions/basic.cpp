e0fdf52cdb70018632d0c130d371253512033b41
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-09-22 08:41:35 +0200

    move viscous outflow bc to separate file

--- src/boundary_conditions/basic.cpp
+++ src/boundary_conditions/basic.cpp
@@ -17,40 +17,6 @@
 namespace boundary_conditions
 {
 
-void viscous_outflow_boundary_inner(t_data &data)
-{
-    if (CPU_Rank != 0) {
-		return;
-	}
-
-	t_polargrid &sig = data[t_data::SIGMA];
-	t_polargrid &e = data[t_data::ENERGY];
-	t_polargrid &vr = data[t_data::V_RADIAL];
-	t_polargrid &visc = data[t_data::VISCOSITY];
-
-	const unsigned int Naz = sig.get_max_azimuthal();
-
-	for (unsigned int naz = 0; naz <= Naz; ++naz) {
-	    sig(0, naz) = sig(1, naz);
-	    e(0, naz) = e(1, naz);
-	}
-
-	const double s = parameters::viscous_outflow_speed;
-
-	#pragma omp parallel for
-	for (unsigned int naz = 0; naz <= Naz; ++naz) {
-
-	    const double Nu0 = visc(0, naz);
-	    const double Nu1 = visc(1, naz);
-	    const double Nu = 0.5 * (Nu0 + Nu1);
-
-	    // V_rad =  - 1.5 / r * Nu (Kley, Papaloizou and Ogilvie, 2008)
-	    vr(1, naz) = -1.5 * s / Rinf[1] * Nu;
-	    vr(0, naz) = -1.5 * s / Rinf[0] * Nu;
-	}
-}
-
-
 static double calc_sig(const double R) {
 	const double sig0 = parameters::sigma0;
 	const double sigexp = -parameters::SIGMASLOPE;
