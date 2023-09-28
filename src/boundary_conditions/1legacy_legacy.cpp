6a4d1ba7e77c19bc6f5f239761ede238bcc73247
Thomas Rometsch <thomas.rometsch@gmail.com>
2023-09-28 09:29:42 +0200

    move old outflow boundary to legacy file

--- src/boundary_conditions/1legacy_legacy.cpp
+++ src/boundary_conditions/1legacy_legacy.cpp
@@ -17,6 +17,74 @@ namespace boundary_conditions
 
 
 /**
+	inner open boundary condition
+*/
+void open_boundary_inner(t_data &data)
+{
+    if (CPU_Rank != 0) {
+	return;
+    }
+
+    t_polargrid &Sigma = data[t_data::SIGMA];
+    t_polargrid &Energy = data[t_data::ENERGY];
+    t_polargrid &vrad = data[t_data::V_RADIAL];
+
+    const unsigned int Naz = Sigma.get_max_azimuthal();
+
+#pragma omp parallel for
+    for (unsigned int naz = 0; naz <= Naz; ++naz) {
+	// copy first ring into ghost ring
+	Sigma(0, naz) = Sigma(1, naz);
+	Energy(0, naz) = Energy(1, naz);
+
+	// set velocity to min(v[+1],0) (allow only outflow)
+	if (vrad(2, naz) > 0.0) {
+	    vrad(1, naz) = 0.0;
+	    vrad(0, naz) = 0.0;
+	} else {
+	    vrad(1, naz) = vrad(2, naz);
+	    vrad(0, naz) = vrad(2, naz);
+	}
+    }
+}
+
+/**
+	outer open boundary condition
+ */
+void open_boundary_outer(t_data &data)
+{
+    if (CPU_Rank != CPU_Highest) {
+	return;
+    }
+
+    t_polargrid &Sigma = data[t_data::SIGMA];
+    t_polargrid &Energy = data[t_data::ENERGY];
+    t_polargrid &vrad = data[t_data::V_RADIAL];
+
+    const unsigned int Nrad = Sigma.get_max_radial();
+    const unsigned int Nrad_v = vrad.get_max_radial();
+    const unsigned int Naz = Sigma.get_max_azimuthal();
+
+#pragma omp parallel for
+    for (unsigned int naz = 0; naz <= Naz; ++naz) {
+	// copy last ring into ghost ring
+	Sigma(Nrad, naz) = Sigma(Nrad - 1, naz);
+	Energy(Nrad, naz) = Energy(Nrad - 1, naz);
+
+	// set velocity to min(v[+1],0) (allow only outflow)
+	if (vrad(Nrad_v - 2, naz) < 0.0) {
+	    vrad(Nrad_v - 1, naz) = 0.0;
+	    vrad(Nrad_v, naz) = 0.0;
+	} else {
+	    vrad(Nrad_v - 1, naz) = vrad(Nrad_v - 2, naz);
+	    vrad(Nrad_v, naz) = vrad(Nrad_v - 2, naz);
+	}
+    }
+}
+
+
+
+/**
 	\param VRadial radial velocity polar grid
 	\param Density density polar grid
 	\param Energy energy polar grid
